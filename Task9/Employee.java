03b44d2c6c1728f0238435a8e62b4bbefb10ce11
L.Ganesh reddy <vtu26458@veltech.edu.in>
2026-03-05 10:26:47 +0530

    Add files via upload

---
+++ Task9/Employee.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    private int id = 101;
+    private String name = "Ganesh";
+    private String department = "IT";
+
+    public void displayEmployee() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
---
+++ Task9/employee.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+
+<html>
+<head>
+<title>Employee Details</title>
+</head>
+
+<body>
+
+<h2>Employee Information</h2>
+
+<p>ID: ${id}</p>
+<p>Name: ${name}</p>
+<p>Department: ${dept}</p>
+
+</body>
+</html>
