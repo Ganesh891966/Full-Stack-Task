537790f126a38b44162f94e92f851594ff0b3356
L.Ganesh reddy <vtu26458@veltech.edu.in>
2026-03-25 09:01:17 +0530

    Delete membership-engine directory

--- membership-engine/src/main/java/org/example/membership/repository/PlanRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.membership.repository;
-
-import org.example.membership.entity.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
--- membership-engine/src/main/java/org/example/membership/repository/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.membership.repository;
-
-import org.example.membership.entity.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
